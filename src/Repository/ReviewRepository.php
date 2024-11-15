5d8c120e308b57b010f360587116d0fcf3a15d1d
Rhys Gregory <88214579+itsrhys754@users.noreply.github.com>
2024-11-15 22:34:42 +0000

    Update ReviewRepository.php

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -5,6 +5,8 @@ namespace Rhys\ReviewBundle\Repository;
 use Rhys\ReviewBundle\Entity\Review;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
+
 
 /**
  * @extends ServiceEntityRepository<Review>
@@ -14,6 +16,20 @@ class ReviewRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Review::class);
+    }
+
+    public function findPendingReviews(?User $excludeUser = null): array
+    {
+        $qb = $this->createQueryBuilder('r')
+            ->where('r.approved = :approved')
+            ->setParameter('approved', false);
+        
+        if ($excludeUser) {
+            $qb->andWhere('r.user != :excludeUser')
+                ->setParameter('excludeUser', $excludeUser);
+        }
+
+        return $qb->getQuery()->getResult();
     }
 
 //    /**

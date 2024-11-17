83a68d4bfbaedd6e22ff4cb77a4df45fdb18d499
Rhys Gregory <88214579+itsrhys754@users.noreply.github.com>
2024-11-17 17:55:11 +0000

    Create VoteRepository.php

---
+++ src/Repository/VoteRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Rhys\ReviewBundle\Repository;
+
+use Rhys\ReviewBundle\Entity\Vote;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+/**
+    * @extends ServiceEntityRepository<Vote>
+ */
+class VoteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Vote::class);
+    }
+
+
+
+//    /**
+//     * @return Vote[] Returns an array of Vote objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Vote
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
